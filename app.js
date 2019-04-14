e7c8b33f39b3b2c017043ca7dd1f7105969c7027
FranticBomb <45299396+FranticBomb@users.noreply.github.com>
2019-04-14 21:46:59 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+(function() {
+	var video = document.getElementById('video'),
+		vendorUrl = window.URL || window.webkitURL;
+
+		navigator.getMedia = navigator.getUserMedia || 
+							 navigator.webkitGetUserMedia ||
+							 navigator.mozGetUserMedia ||
+							 navigator.msGetUserMedia;
+
+	// Capture video
+	navigator.getMedia({
+		video: true,
+		audio: false
+	}, function(stream) {
+		console.log(stream);
+	}, 
+	   function(error) {
+	  // An error occured
+	  // error.code 	
+	});
+	
+
+})();
